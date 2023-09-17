dfd8f0ab1f5056c738c4f38a061a9af117a50c1e
moesama <moesama_zzt@163.com>
2023-09-18 00:58:01 +0800

    :sparkles: FEAT: success code

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"go-more-protocol/config"
 	"go-more-protocol/proto/ws"
+	"os"
+	"os/signal"
+	"time"
 )
 
-func main() {
+func init() {
 	ws.Init()
 	config.Test()
+}
+
+func main() {
+	c := make(chan os.Signal)
+	signal.Notify(c)
+	go func() {
+		fmt.Println("Go routine running")
+		time.Sleep(3 * time.Second)
+		fmt.Println("Go routine done")
+	}()
+	<-c
+	fmt.Println("bye")
 }
